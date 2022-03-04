fa4bf8a8b757eefeab0043a12f402245d2b63385
Nandithh <98866734+Nandithh@users.noreply.github.com>
2022-03-04 12:31:04 +0530

    commit

--- 3_Implementation/inc/BoreMotor.h
+++ 3_Implementation/inc/BoreMotor.h
@@ -1,6 +1,19 @@
+/**
+ * @file BoreMotor.h
+ * @author nandith
+ * @brief header file
+ * @version 0.1
+ * @date 2022-03-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef __BoreMotor_H__
 #define __BoreMotor_H__
-
+/**
+ * @brief Functions of different brands of motor
+ * 
+ */
 void Texmo();
 void Kirloskar();
 void CRI();
