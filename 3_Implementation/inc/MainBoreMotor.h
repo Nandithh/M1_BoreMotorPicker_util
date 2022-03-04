dbb02c171a8aeca1a339f179eef606cfd3c865c6
Nandithh <98866734+Nandithh@users.noreply.github.com>
2022-03-04 12:30:14 +0530

    commit

--- 3_Implementation/inc/MainBoreMotor.h
+++ 3_Implementation/inc/MainBoreMotor.h
@@ -1,10 +1,23 @@
+/**
+ * @file MainBoreMotor.h
+ * @author nandith
+ * @brief header file
+ * @version 0.1
+ * @date 2022-03-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef __MainBoreMotor_H__
 #define __MainBoreMotor_H__
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+/**
+ * @brief structure is used to get customer details
+ * 
+ */
 
 struct BoreMotor
 {
@@ -16,4 +29,3 @@ struct BoreMotor
 
 struct BoreMotor B;
 #endif
-
